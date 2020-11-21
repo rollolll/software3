2ccdc8a4b002ed82bbd4d24fe84323baa3902d14
rollolll <j-mario9715@hotmail.com>
2020-11-21 16:03:56 -0500

    segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { KpasapexComponent } from './kpasapex/kpasapex.component';
+import { CrudComponent } from './crud/crud.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    KpasapexComponent
+    KpasapexComponent,
+    CrudComponent
   ],
   imports: [
     BrowserModule,
